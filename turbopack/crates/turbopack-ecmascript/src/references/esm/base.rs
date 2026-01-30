809d7b7ddf03117c6ece26d6a5bfaea2ec136296
Tobias Koppers <tobias.koppers@googlemail.com>
2026-01-30 09:04:08 +0100

    Turbopack: Tree Shaking imports only internal

--- turbopack/crates/turbopack-ecmascript/src/references/esm/base.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/esm/base.rs
@@ -17,6 +17,7 @@ use turbopack_core::{
         ChunkableModuleReference, ChunkingContext, ChunkingType, ChunkingTypeOption,
         ModuleChunkItemIdExt,
     },
+    context::ProcessResult,
     issue::{
         Issue, IssueExt, IssueSeverity, IssueSource, IssueStage, OptionIssueSource,
         OptionStyledString, StyledString,
@@ -437,11 +438,15 @@ impl ModuleReference for EsmAssetReference {
                 && module.is_match(TURBOPACK_PART_IMPORT_SOURCE)
             {
                 if let Some(part) = &self.export_name {
-                    return Ok(*ModuleResolveResult::module(ResolvedVc::upcast(
-                        EcmascriptModulePartAsset::select_part(*self.module, part.clone())
-                            .to_resolved()
-                            .await?,
-                    )));
+                    let ProcessResult::Module(module_part) =
+                        *EcmascriptModulePartAsset::select_part(*self.module, part.clone()).await?
+                    else {
+                        bail!(
+                            "ModulePart::Internal should always return a module part in \
+                             select_part"
+                        );
+                    };
+                    return Ok(*ModuleResolveResult::module(module_part));
                 }
                 bail!("export_name is required for part import")
             }
